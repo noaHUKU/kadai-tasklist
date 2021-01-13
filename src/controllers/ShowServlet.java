0ddb5740ea519a59e629a768a1dc168f3beb39ab
noa.yamamoto <n.huku456@gmail.com>
2021-01-13 23:47:52 +0900

    Add ShowServlet

---
+++ src/controllers/ShowServlet.java
@@ -0,0 +1,49 @@
+//index ページのID番号のリンクのクリックで該当のタスクの詳細情報を表示するサーブレット
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class ShowServlet
+ */
+@WebServlet("/show")
+public class ShowServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        // 該当のIDのタスク1件のみをデータベースから整数値に変換して取得
+        Task m = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        // メッセージデータをリクエストスコープにセットしてshow.jspを呼び出す
+        request.setAttribute("task", m);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/taskss/show.jsp");
+        rd.forward(request, response);
+    }
+
+}
